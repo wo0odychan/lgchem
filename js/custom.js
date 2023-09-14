8f48a64eb11f0ba82f3d6455f6d4189f28f61c37
woojin <133613646+wo0odychan@users.noreply.github.com>
2023-09-14 17:54:32 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -40,11 +40,60 @@ $(function () {
         $('.tab_inner li').eq(idx).addClass('on').siblings().removeClass('on');
     });
 
+    $('.main_business .box').on('mouseenter', function () {
+        $(this).addClass('on').siblings().removeClass('on');
+    });
+
+    const newsSlide = new Swiper('.news_slide', {
+        loop: false,
+        autoplay: false,
+        on: {
+            slideChangeTransitionStart: function () {
+                $('.news_slide .dots li')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on');
+                $('.news_slide .arrows>div')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on');
+            }
+        }
+    });
+
+    // 1번째
+    $('.news_slide .arrows .left').on('click', function () {
+        newsSlide.slidePrev();
+    });
+
+    // 2번째
+    $('.news_slide .arrows .right').on('click', function () {
+        newsSlide.slideNext();
+    });
+
+    // dots
+
+    $('.news_slide .dots li').on('click', function () {
+        const idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on')
+        newsSlide.slideTo(idx);
+    });
+
+    $('.glb').on('click', function () {
+        $(this).toggleClass('on')
+    })
+    $('.btn_l').on('click', function () {
+        $(this).toggleClass('on')
+    })
+
     $('.mobile_btn').on('click', function () {
         $(this).toggleClass('on');
         $('.gnb').toggleClass('on');
-
     });
+
+
     $('.gnb').on('wheel touchmove', function (e) {
         e.preventDefault();
     });
